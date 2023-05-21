ced714b34b88d2187813638ba0c9d635cda0e310
Vladimir <shokilak@gmail.com>
2023-05-21 17:00:32 +0600

    feat: create right direct

--- src/infrastructure/postgre/data-source.ts
+++ src/infrastructure/postgre/data-source.ts
@@ -2,15 +2,20 @@ import { DataSource } from 'typeorm';
 import 'dotenv/config';
 
 const myDataSource = new DataSource({
-    type: 'postgres',
+    type: 'postgre',
     host: process.env.DATABASE_HOST.toString(),
     port: Number(process.env.DATABASE_PORT),
     username: process.env.POSTGRES_USERNAME.toString(),
+    driver: undefined,
+    location: "",
+    region: "",
+    resourceArn: "",
+    secretArn: "",
     password: process.env.DATABASE_PASSWORD.toString(),
     database: process.env.POSTGRES_DATABASE.toString(),
     // entities: entities,
     migrations: [__dirname + '/migrations/*{.ts,.js}'],
-    synchronize: false,
+    synchronize: false
 });
 
 export default myDataSource;
