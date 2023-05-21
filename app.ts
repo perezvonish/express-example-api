7962d2bc124e659ce49ebc4fdfe9881a95c505ef
Vladimir <shokilak@gmail.com>
2023-05-21 18:18:17 +0600

    fix: right import cors()

--- app.ts
+++ app.ts
@@ -1,7 +1,7 @@
 import * as dotenv from "dotenv"
 dotenv.config();
 import * as express from "express"
-import cors from 'cors';
+import * as cors from 'cors';
 
 import routes from "./src/config/controllers";
 import {initMongo} from "./src/infrastructure/mongo/connection.source";
@@ -9,10 +9,9 @@ import {Express} from "express";
 
 const app = express();
 
-// app.use(cors);
-
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/auth', routes.authController)
 app.use('/admin', routes.adminController)
@@ -20,7 +19,7 @@ app.use('/users', routes.usersController)
 app.use('/posts', routes.postsController)
 
 async function init(app: Express) {
-    await initMongo();
+    // await initMongo();
 
     app.listen(process.env.SERVER_PORT, () => {
         console.log(`Server started on port --> ${process.env.SERVER_PORT}`)
