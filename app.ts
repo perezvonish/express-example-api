6028a7b78326d9bdee371cc65a986b4e6d709022
Vladimir <shokilak@gmail.com>
2023-05-21 17:38:21 +0600

    feat: renamed

--- app.ts
+++ app.ts
@@ -1,22 +1,24 @@
 import * as dotenv from "dotenv"
 dotenv.config();
-import express from "express"
+import * as express from "express"
 import cors from 'cors';
 
 import routes from "./src/config/controllers";
-import postsController from "./src/controllers/posts.controller";
+import {initMongo} from "./src/infrastructure/mongo/connection.source";
 
 const app = express();
 
-app.use(cors);
+// app.use(cors);
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
 
 app.use('/auth', routes.authController)
 app.use('/admin', routes.adminController)
 app.use('/users', routes.usersController)
 app.use('/posts', routes.postsController)
+
+// initMongo();
 
 app.listen(process.env.SERVER_PORT, () => {
     console.log(`Server started on port --> ${process.env.SERVER_PORT}`)
